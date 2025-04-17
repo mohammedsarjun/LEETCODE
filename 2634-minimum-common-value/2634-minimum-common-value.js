720320e95a227bb494ca63cbbcd627726b6cbbc3
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-04-17 10:01:44 +0530

    Time: 19 ms (22.72%), Space: 75.1 MB (13.31%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.js
+++ 2634-minimum-common-value/2634-minimum-common-value.js
@@ -4,13 +4,10 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-let set=new Set(nums1)
-
-for(let num of nums2){
-    if(set.has(num)){
-        return num
+   const set = new Set(nums1);
+    for (let num of nums2) {
+        if (set.has(num)) return num;
     }
-}
 
-return -1
+    return -1 
 };
