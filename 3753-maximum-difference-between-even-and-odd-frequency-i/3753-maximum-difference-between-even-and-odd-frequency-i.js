2f8148c440f9fe56405c6ca2244f90c652174382
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-06-11 09:43:39 +0530

    Time: 46 ms (5.4%), Space: 63.3 MB (5.4%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
@@ -11,7 +11,7 @@ s=s.split('')
     for(let i=0;i<setS.length;i++){
         obj[setS[i]]=s.filter((alp)=>alp==setS[i]).length
     }
-    let result=Object.values(obj).sort((a,b)=>b-a)
+    let result=Object.values(obj)
     let odd=Math.max(...result.filter((num)=>num%2!=0))
     let even=Math.min(...result.filter((num)=>num%2==0))
        return odd-even
