f65a3ea76c365cb84c58d27a9204a8dfcbf81462
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-06-13 09:26:58 +0530

    Time: 1041 ms (58.26%), Space: 54.9 MB (66.38%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    let count=0
+    for(let i=1;i<=num/2;i++){
+        if(num%i==0){
+            count+=i
+        }
+
+    }
+    console.log(count)
+
+    return count==num
+};
