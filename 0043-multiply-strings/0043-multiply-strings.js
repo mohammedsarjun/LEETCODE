11baae1193a3a4c63c59b6263a2e5dcb3f5ac36d
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-06-19 10:09:07 +0530

    Time: 0 ms (100%), Space: 52.6 MB (99.13%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.js
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -4,35 +4,6 @@
  * @return {string}
  */
 var multiply = function(num1, num2) {
-    let num1Arr=[]
-    for(let i=0;i<num1.length;i++){
-        num1Arr[i]=[]
-        for(let j=0;j<num1[i];j++){
-               num1Arr[i].push(0)
-        }
-     
-    }
-    let num2Arr=[]
-    for(let i=0;i<num2.length;i++){
-        num2Arr[i]=[]
-        for(let j=0;j<num2[i];j++){
-               num2Arr[i].push(0)
-        }
-     
-    }
 
-    let res1=""
-
-        for(let i=0;i<num1Arr.length;i++){
-          
-            res1+=num1Arr[i].length
-     
-    }
-
-    let res2=""
-       for(let i=0;i<num2Arr.length;i++){
-            res2+=num2Arr[i].length
-    }
-
-    return String(BigInt(res1) * BigInt(res2));
+    return String(BigInt(num1) * BigInt(num2));
 };
