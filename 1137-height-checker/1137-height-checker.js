e5cf554211546eeca773bfac4a6e7161db98ecbe
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-07-22 09:55:41 +0530

    Time: 1 ms (72.42%), Space: 55 MB (30.46%) - LeetHub

--- 1137-height-checker/1137-height-checker.js
+++ 1137-height-checker/1137-height-checker.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var heightChecker = function(heights) {
-    let sorted=JSON.stringify(heights)
-    sorted=JSON.parse(sorted)
-    sorted.sort((a,b)=>a-b)
+    let expected=structuredClone(heights).sort((a,b)=>a-b)
     let count=0
     for(let i=0;i<heights.length;i++){
-        if(heights[i]!=sorted[i]){
-            count++
-        }
+        if(heights[i]!=expected[i]) count++
     }
+
     return count
+
 };
