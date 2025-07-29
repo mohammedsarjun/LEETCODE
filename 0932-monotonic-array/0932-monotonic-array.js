5a028f9a3e4e70c5a577aef14e7086ddef9c894a
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-07-29 09:41:26 +0530

    Time: 3 ms (48.09%), Space: 67.1 MB (47.33%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+
+    let inc=false
+    let dec=false
+    for(let i=0;i<nums.length;i++){
+       
+            if(nums[i]<nums[i+1]) inc=true
+   
+            if(nums[i]>nums[i+1]) dec=true
+        
+    }
+
+    return !(inc && dec)
+};
