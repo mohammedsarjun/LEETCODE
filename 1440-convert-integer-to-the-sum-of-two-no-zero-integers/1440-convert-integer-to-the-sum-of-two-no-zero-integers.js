6e82f11eea023f7f8bd33ee860300eb0425a6bf5
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-08-01 10:04:34 +0530

    Time: 0 ms (100%), Space: 52.6 MB (89.58%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -3,20 +3,24 @@
  * @return {number[]}
  */
 var getNoZeroIntegers = function(n) {
-    let arr=[Math.floor(n/2),Math.floor(n/2)]
-    if(arr.reduce((acc,curr)=>acc+curr,0)!=n){
-        arr[1]=arr[1]+1
-    }
 
-    while(String(arr[0]).includes('0')||String(arr[1]).includes('0')){
-        if(String(arr[0]).includes('0')){
-        arr[0]=arr[0]-1
-        arr[1]=arr[1]+1
-    }else if(String(arr[1]).includes('0')){
-         arr[0]=arr[0]-1
-        arr[1]=arr[1]+1
-    }
-    }
-    
-    return arr
+let a=--n
+let b=1
+
+while(isZeroContain(a)||isZeroContain(b)){
+    a--
+    b++
+}
+
+return [a,b]
 };
+
+
+function isZeroContain(num){
+    num=String(num)
+    return num.includes("0")
+}
+function isInteger(num){
+    num=String(num)
+    return !num.includes(".")
+}
