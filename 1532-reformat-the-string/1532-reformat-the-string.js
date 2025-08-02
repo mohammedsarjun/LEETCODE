2ce77e7b3cb1b6460b7c28f64ad2cfd05b314d19
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-08-02 09:58:22 +0530

    Time: 56 ms (5.33%), Space: 63.5 MB (5.33%) - LeetHub

---
+++ 1532-reformat-the-string/1532-reformat-the-string.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reformat = function (s) {
+    s=s.split('')
+    let num=s.filter((num)=>num>-Infinity)
+    let str=s.filter((str)=>!num.includes(str))
+    let diff=Math.abs(num.length-str.length)
+    if(diff>1) return ""
+
+    let result=[]
+console.log(num,str)
+ while(true){
+        result.push(num.shift())
+        result.push(str.shift())
+        if(!str.length || !num.length) break
+    }
+
+    if(str.length) result.unshift(str.shift())
+    if(num.length) result.push(num.shift())
+  return result.join('')
+};
