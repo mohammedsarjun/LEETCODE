080be976e258ed43259d1937d5e3937a769f30d9
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-08-28 09:27:49 +0530

    Time: 85 ms (38.81%), Space: 63.7 MB (60.45%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.js
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.js
@@ -4,26 +4,29 @@
  * @return {number}
  */
 var countCharacters = function(words, chars) {
-    chars=chars.split('')
-    let count=0
-    let cloneChar=structuredClone(chars)
-    for(let i=0;i<words.length;i++){
-        let tempCount=0
+    let obj={}
+    for(let i=0;i<chars.length;i++){
+        obj[chars[i]]=(obj[chars[i]]||0)+1
 
+    }
+    let result=0
+
+    for(let i=0;i<words.length;i++){
+        let tempObj=structuredClone(obj)
+        
         for(let j=0;j<words[i].length;j++){
-            if(chars.includes(words[i][j])){
-         
-                tempCount++
-                chars.splice(chars.indexOf(words[i][j]),1)
-                
-            }else{
-                tempCount=0
+          
+            if(!tempObj[words[i][j]]){
                 break
+            }else{
+                tempObj[words[i][j]]--
+            }
+
+            if(j==words[i].length-1){
+                result+=words[i].length
             }
         }
-        count+=tempCount
-        chars=[...cloneChar]
     }
 
-    return count
+   return result
 };
