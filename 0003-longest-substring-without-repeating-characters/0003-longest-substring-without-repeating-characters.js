e689adc66b083dda45a744107e0598f75fabb1b6
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-09-06 10:41:04 +0530

    Time: 23 ms (20.82%), Space: 59.7 MB (31.61%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+let longSubWindow=-Infinity
+let right=0
+let obj={}
+for(let left=0;left<s.length;left++){
+
+while(!obj[s[right]]&&right<s.length){
+obj[s[right]]=1
+
+right++
+
+}
+longSubWindow=Math.max(longSubWindow,Math.abs(left-right))
+
+
+obj[s[left]]=undefined
+}
+return longSubWindow==-Infinity?0:longSubWindow
+};
