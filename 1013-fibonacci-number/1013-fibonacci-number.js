9733906162c763d2913d7130117eec9e7ca1c904
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-09-08 10:20:40 +0530

    Time: 53 ms (54.69%), Space: 53.5 MB (56.32%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -6,8 +6,8 @@ var fib = function(n) {
     if(n==0) return n
     let result=0
     let arr=[0,1]
-   for(let i=2;i<n;i++){
+   for(let i=2;i<=n;i++){
     arr.push(arr[arr.length-1]+arr[arr.length-2])
    } 
-  return arr[arr.length-1]+arr[arr.length-2]
+  return arr[n]
 };
