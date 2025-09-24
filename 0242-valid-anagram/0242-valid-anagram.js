b3001de0855c1d5a4f12cc3751a154cd6c78bcc0
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-09-24 10:27:01 +0530

    Time: 27 ms (26.57%), Space: 58.5 MB (43.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,8 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-
-    console.log(s.split('').sort(),t.split('').sort())
-   return s.split('').sort().join('')==t.split('').sort().join('')?true:false
-
+    return s.split('').sort().join('')==t.split('').sort().join('')
 };
