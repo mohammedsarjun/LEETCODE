05aae6ec2514ac947710c947a0a88e9170f98063
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-10-22 15:35:35 +0530

    Time: 22 ms (46.8%), Space: 63.3 MB (19.46%) - LeetHub

---
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+    for(let i=0;i<words.length;i++){
+        if(words[i].split('').reverse().join('')==words[i]) return words[i]
+    }
+
+    return ""
+};
