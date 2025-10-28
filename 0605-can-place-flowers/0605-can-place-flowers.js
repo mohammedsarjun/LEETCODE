a95980c969312d854c2103b5daac74308a27af85
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-10-28 15:33:55 +0530

    Time: 0 ms (100%), Space: 56.1 MB (58.23%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    
+if(!n)return true
+    for(let i=0;i<flowerbed.length;i++){
+        if(!flowerbed[i-1]&&!flowerbed[i+1]&&!flowerbed[i]){
+            flowerbed[i]=1
+            n--
+            if(!n) return true
+        }
+    }
+
+
+    return false
+};
