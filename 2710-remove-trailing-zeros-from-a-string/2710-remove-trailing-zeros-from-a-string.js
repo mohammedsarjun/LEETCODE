dd8478d167e99adda657ac633186d266dcebca7b
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2025-11-24 17:31:20 +0530

    Time: 2 ms (65.99%), Space: 56.1 MB (78.23%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
@@ -8,15 +8,14 @@ var removeTrailingZeros = function (num) {
     let isTrailZeroAvailable = num[num.length - 1]
 
     if (isTrailZeroAvailable != 0) return num
-
+    num=num.split('')
     for (let i = num.length - 1; i >= 0; i--) {
         if (num[i] != 0) {
-            breakIndex=i
             break
         }
-        count++
+        num.pop()
+
     }
-    let arrNum=num.split('')
-    arrNum.splice(breakIndex+1,count)
-    return arrNum.join('')
+
+    return num.join('')
 };
