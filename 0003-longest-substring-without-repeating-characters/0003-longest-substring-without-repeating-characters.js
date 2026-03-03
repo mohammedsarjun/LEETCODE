782aff0ec473cd7f487e63f6efba66962742df88
Sarjun <151373961+mohammedsarjun@users.noreply.github.com>
2026-03-03 14:22:09 +0530

    Time: 7 ms (56.35%), Space: 59 MB (46.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let maxSubString = 0
+
     let l = 0
-    let hashTable = {}
+    let vis=new Set()
+    let max=0
 
-    for (let r = 0; r < s.length; r++) {
+    for(let r=0;r<s.length;r++){
 
-        while (hashTable[s[r]]) {
-            delete hashTable[s[l]]
+        while(vis.has(s[r])){
+            vis.delete(s[l])
             l++
         }
-
-        hashTable[s[r]] = 1
-        maxSubString = Math.max(maxSubString, r-l+1)
+        vis.add(s[r])
+        max=Math.max(max,r-l+1)
     }
-
-    return maxSubString
+    return max
 };
